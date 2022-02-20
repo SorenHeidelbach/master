9d52c8be9d97cce4d4c4fff24eff9025f7152aae
SorenHeidelbach <sheide17@student.aau.dk@a1024-gc1-b-02.srv.aau.dk>
2022-02-20 21:16:34 +0100

    PCR to NAT comparison u-test and mean diff

--- scripts/current_difference/megalodon_signal_map.R
+++ scripts/current_difference/megalodon_signal_map.R
@@ -1,5 +1,5 @@
 #!/usr/bin/Rscript
-.libPaths( c( "/shared-nfs/SH/code/Rlib" , .libPaths()))
+.libPaths( c( "/shared-nfs/SH/software/Rlib" , .libPaths()))
 setwd(here::here())
 pacman::p_load(
   "data.table",
@@ -15,7 +15,7 @@ pacman::p_load(
   "logger"
 )
 
-  
+
 
 group_nest_dt <- function(dt, ..., .key = "data"){
   stopifnot(is.data.table(dt))
@@ -37,8 +37,6 @@ arg$reference <- "/shared-nfs/SH/samples/zymo/Zymo-Isolates-SPAdes-Illumina.fast
 arg$signal_mapping_pcr <- "/shared-nfs/SH/samples/zymo/megalodon/pcr_test/signal_mappings.hdf5"
 arg$signal_mapping_nat <- "/shared-nfs/SH/samples/zymo/megalodon/nat_test2/signal_mappings.hdf5"
 
-arg$batch <- "Batch_0"
-
 ###############################################################################
 # Importing signal_mapping.HDF5 
 ###############################################################################
@@ -46,41 +44,37 @@ arg$batch <- "Batch_0"
 # Load in reference
 ref <- read.fasta(arg$reference)
 
-load_mapping_hdf5 <- function(arg_mapping, reads_ids = NA){
-  # Open HDF5
-  hdf5_file <- H5Fopen(arg_mapping)
-  
-  # h5ls(hdf5_file)
-  
+load_mapping_hdf5 <- function(hdf5_file, batch, reads_ids = NA){
   # Read dacs (current measurements)
   log_info("Loading data from {batch}")
+  dacs <- h5read(hdf5_file, name = paste0("/Batches/", batch, "/Dacs")) %>% 
     data.table() %>% 
     setnames("dacs")
   
   # Read Ref_to_signal (information of number of dacs mapping to each ref position)
-  Ref_to_signal <- h5read(hdf5_file, name = paste0("/Batches/", arg$batch, "/Ref_to_signal")) %>% 
+  Ref_to_signal <- h5read(hdf5_file, name = paste0("/Batches/", batch, "/Ref_to_signal")) %>% 
     data.table() %>% 
     setnames("ref_to_signal")
   
   # Read Reference (Interger representation of read bases)
-  Reference = h5read(hdf5_file, name = paste0("/Batches/", arg$batch, "/Reference")) %>% 
+  Reference = h5read(hdf5_file, name = paste0("/Batches/", batch, "/Reference")) %>% 
     data.table() %>% 
     setnames("reference")
   
   # Read read information (mainly ID, dacs length, Ref/sig length)
-  reads <- h5read(hdf5_file, name = paste0("/Batches/", arg$batch, "/Dacs_lengths")) %>% 
+  reads <- h5read(hdf5_file, name = paste0("/Batches/", batch, "/Dacs_lengths")) %>% 
     data.table() %>% 
     setnames("Dacs_lengths") %>% 
-    cbind("Ref_to_signal_lengths" = h5read(hdf5_file, name = paste0("/Batches/", arg$batch, "/Ref_to_signal_lengths"))) %>% 
-    cbind("Reference_lengths" = h5read(hdf5_file, name = paste0("/Batches/", arg$batch, "/Reference_lengths"))) %>% 
-    #cbind("digitisation" = h5read(hdf5_file, name = paste0("/Batches/", arg$batch, "/digitisation"))) %>% 
-    cbind("offset" = h5read(hdf5_file, name = paste0("/Batches/", arg$batch, "/offset"))) %>% 
-    cbind("range" = h5read(hdf5_file, name = paste0("/Batches/", arg$batch, "/range"))) %>% 
-    #cbind("scale_frompA" = h5read(hdf5_file, name = paste0("/Batches/", arg$batch, "/scale_frompA"))) %>% 
-    #cbind("shift_frompA" = h5read(hdf5_file, name = paste0("/Batches/", arg$batch, "/shift_frompA"))) %>% 
+    cbind("Ref_to_signal_lengths" = h5read(hdf5_file, name = paste0("/Batches/", batch, "/Ref_to_signal_lengths"))) %>% 
+    cbind("Reference_lengths" = h5read(hdf5_file, name = paste0("/Batches/", batch, "/Reference_lengths"))) %>% 
+    #cbind("digitisation" = h5read(hdf5_file, name = paste0("/Batches/", batch, "/digitisation"))) %>% 
+    cbind("offset" = h5read(hdf5_file, name = paste0("/Batches/", batch, "/offset"))) %>% 
+    cbind("range" = h5read(hdf5_file, name = paste0("/Batches/", batch, "/range"))) %>% 
+    #cbind("scale_frompA" = h5read(hdf5_file, name = paste0("/Batches/", batch, "/scale_frompA"))) %>% 
+    #cbind("shift_frompA" = h5read(hdf5_file, name = paste0("/Batches/", batch, "/shift_frompA"))) %>% 
     apply(MARGIN = 2, FUN = as.numeric) %>% 
     data.table(stringsAsFactors = FALSE) %>% 
-    cbind("read_id" = h5read(hdf5_file, name = paste0("/Batches/", arg$batch, "/read_id")))
+    cbind("read_id" = h5read(hdf5_file, name = paste0("/Batches/", batch, "/read_id")))
   H5Fclose(hdf5_file)
   
   log_info("Adding read information to reference mapping")
@@ -143,49 +137,6 @@ load_mapping_hdf5 <- function(arg_mapping, reads_ids = NA){
   return(dacs_test)
 }
 
-# Sanity check for similarity between raw file and dacs
-p_current_v_dac <- list()
-p_current_v_dac <- lapply(
-  reads$read_id,
-  function(x){
-    # Dacs from signal mapping
-    print(x)
-    dac_plot <- dacs_test[
-        read_id == x, 
-      ][
-        , pos := 1:.N
-      ]
-    list(
-      ggplot() +
-        geom_line(
-          data = dac_plot,
-          mapping = aes(x = pos, y = V1, group = 1, ), 
-          size = 0.5
-          ) +
-        # geom_line(
-        #   data = raw_plot,
-        #   mapping = aes(x = pos, y = signal+1000, group = 1), 
-        #   size = 0.5
-        # ) +
-        theme_bw() +
-        ggforce::facet_zoom(x = c(1.8e4, 2e4)),
-      ggplot(Ref_to_signal[read_id == x, ], aes(x = pos, y = signal_frame)) +
-        geom_line( 
-          size = 0.5
-        ) +
-        theme_bw() 
-    )
-  }
-)
-
-group_nest_dt <- function(dt, ..., .key = "data"){
-  stopifnot(is.data.table(dt))
-  by <- substitute(list(...))
-  dt <- dt[, list(list(.SD)), by = eval(by)]
-  setnames(dt, old = "V1", new = .key)
-  dt
-}
-
 mapping_pcr <- fread("/shared-nfs/SH/samples/zymo/megalodon/pcr_test/mappings_sorted_view.txt") %>% 
   select(V1, V3, V4) %>% 
   setnames(c("read_id", "contig", "start"))
@@ -242,16 +193,111 @@ signal_mappings[
   , dacs_norm := (V1 - mean(V1))/sd(V1), by = read_id
 ]
 
-utest <- signal_mappings$data %>% 
-  lapply(
-    function(dt){
-      pcr = dt[type == "pcr",]
-      nat = dt[type == "nat",]
-      if ((nrow(pcr) != 0) & (nrow(nat) != 0)){
-        wilcox.test(nat$V1, pcr$V1)
+signal_mappings <- signal_mappings %>%  group_nest_dt(contig_id, contig_index, contig) 
+
+# p_dac_norm <- signal_mappings[contig_id %in% paste0("bs_contig1_4990", 10:20), ] %>% 
+#   ggplot(aes(x = as.character(contig_index), y = dacs_norm, fill = type)) +
+#   geom_boxplot(outlier.alpha = 0) +
+#   geom_jitter(shape = 21, size = 0.5, position = position_jitterdodge(seed = 2)) +
+#   theme_bw() +
+#   theme(axis.text.x.bottom = element_text(angle = 90))
+# 
+# p_dac <- signal_mappings[contig_id %in% paste0("bs_contig1_4990", 10:20), ] %>% 
+#   ggplot(aes(x = as.character(contig_index), y = V1, fill = type)) +
+#   geom_boxplot(outlier.alpha = 0) +
+#   geom_jitter(shape = 21, size = 0.5, position = position_jitterdodge(seed = 2), ) +
+#   theme_bw() +
+#   theme(axis.text.x.bottom = element_text(angle = 90))
+# 
+# p_dac_norm / p_dac
+library(doParallel)  # will load parallel, foreach, and iterators
+cl <- makeCluster(30)
+registerDoParallel(cl)
+signal_mappings2 <- signal_mappings %>% 
+  left_join(signal_mappings %>%
+    mutate(
+      chunk = floor(contig_index/1000)
+    ) %>% 
+    split(f = .$chunk) %>% 
+    mclapply( 
+      mc.cores = 30,
+      function(x){
+        apply(
+          x,
+          MARGIN = 1,
+          function(dt){
+            pcr = dt$data[type == "pcr",]
+            nat = dt$data[type == "nat",]
+            if ((nrow(pcr) != 0) & (nrow(nat) != 0)){
+              p <- wilcox.test(pcr$dacs_norm, nat$dacs_norm)$p.value
+              list(
+                contig_id = dt$contig_id,
+                p_val = p,
+                dacs_nat_minus_pcr = mean(nat$dacs_norm) - mean(pcr$dacs_norm)
+              )
+            }else {
+              list(
+                contig_id = dt$contig_id,
+                p_val = NA,
+                dacs_nat_minus_pcr = mean(nat$dacs_norm) - mean(pcr$dacs_norm)
+              )
+            }
+          }
+        ) %>% 
+          rbindlist()
       }
-      
-    }
-  )
+    ) %>% 
+      rbindlist()
+)
+
+arg$min_u_val <- 1e-8
+
+p_mean_dif_event <- signal_mappings2 %>% 
+  filter(!is.na(p_val)) %>% 
+  mutate(
+    event = ifelse(p_val < arg$min_u_val, TRUE, FALSE)
+  ) %>% 
+  ggplot(aes(x = contig_index, y = dacs_nat_minus_pcr)) +
+  geom_hline(yintercept = 0) +
+  geom_segment(aes(x = contig_index, xend = contig_index, yend = dacs_nat_minus_pcr), y = 0, size = 0.2) +
+  geom_point(aes(fill = event, size = event), shape = 21) +
+  scale_size_manual(values = c(0.9, 3)) +
+  guides(
+    fill = guide_legend(title = "Event"),
+    size = guide_legend(title = "Event")
+  ) +
+  labs(
+    x = "Contig Position",
+    y = "NAT vs. PCR (Mean difference)"
+  ) #+ ggforce::facet_zoom(xlim = c(218500, 219500))
+
+p_pval_event <- signal_mappings2 %>% 
+  filter(!is.na(p_val)) %>% 
+  mutate(
+    event = ifelse(p_val < arg$min_u_val, TRUE, FALSE)
+  ) %>% 
+  ggplot(aes(x = contig_index, y = p_val)) +
+  geom_hline(yintercept = 1) +
+  geom_segment(aes(x = contig_index, xend = contig_index, yend = p_val), y = 0, size = 0.2) +
+  geom_point(aes(fill = event, size = event), shape = 21) +
+  scale_size_manual(values = c(0.9, 3)) +
+  scale_y_log10() +
+  guides(
+    fill = guide_legend(title = "Event"),
+    size = guide_legend(title = "Event")
+  ) +
+  labs(
+    x = "Contig Position",
+    y = "NAT vs. PCR (U-value)"
+  ) #+ggforce::facet_zoom(xlim = c(218500, 219500))
+p_work <- (p_pval_event | p_mean_dif_event) +
+  plot_layout(guides = "collect") +
+  plot_annotation(
+    title = paste0("Significant events in ", contig_select, " (U-value threshold: ", arg$min_u_val, ")")
+  )  &
+  theme_bw()
+p_work
+
+
 
 
