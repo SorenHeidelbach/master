f6731efb4152903c5ce90b9aebb96202aa5d5ac1
SorenHeidelbach <sheide17@student.aau.dk@a1024-gc1-b-02.srv.aau.dk>
2022-02-20 21:04:30 +0100

    Started using 'logger'

--- scripts/current_difference/megalodon_signal_map.R
+++ scripts/current_difference/megalodon_signal_map.R
@@ -10,10 +10,27 @@ pacman::p_load(
   "microbenchmark",
   "foreach",
   "Rsamtools",
-  "stringr"
+  "stringr",
+  "patchwork",
+  "logger"
 )
 
   
+
+group_nest_dt <- function(dt, ..., .key = "data"){
+  stopifnot(is.data.table(dt))
+  by <- substitute(list(...))
+  dt <- dt[, list(list(.SD)), by = eval(by)]
+  setnames(dt, old = "V1", new = .key)
+  dt
+}
+unnest_dt <- function(dt, col, id){
+  stopifnot(is.data.table(dt))
+  by <- substitute(id)
+  col <- substitute(unlist(col, recursive = FALSE))
+  dt[, eval(col), by = eval(by)]
+}
+
 
 arg <- list()
 arg$reference <- "/shared-nfs/SH/samples/zymo/Zymo-Isolates-SPAdes-Illumina.fasta"
